c28a9404800cefd844e2eb93a4069d79b755a67b
Anwesh43 <anweshthecool0@gmail.com>
2018-12-14 04:05:45 +0530

    adding drawLPSNode canvas extension method

--- linepikesstepview/src/main/java/com/anwesh/uiprojects/linepikesstepview/LinePikesStepView.kt
+++ linepikesstepview/src/main/java/com/anwesh/uiprojects/linepikesstepview/LinePikesStepView.kt
@@ -28,3 +28,32 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
+
+fun Canvas.drawLPSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val size : Float = gap / sizeFactor
+    val xGap = size / (lines + 1)
+    paint.color = color
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * (i + 1), h/2)
+    for (j in 0..1) {
+        val scj : Float = sc1.divideScale(j, 2)
+        save()
+        rotate(j * -90f * (1 - sc2))
+        for (k in 0..(lines - 1)) {
+            val sck : Float = scj.divideScale(k, lines)
+            save()
+            translate(xGap * (k + 1), 0f)
+            drawLine(0f, 0f, 0f, -xGap * sck, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
