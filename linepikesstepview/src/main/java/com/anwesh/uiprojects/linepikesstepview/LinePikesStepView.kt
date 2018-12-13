7a4811d3ef7b41903311aa52f81ad3b21934b0a1
Anwesh43 <anweshthecool0@gmail.com>
2018-12-14 04:07:07 +0530

    adding LinePilkesStepView class

--- linepikesstepview/src/main/java/com/anwesh/uiprojects/linepikesstepview/LinePikesStepView.kt
+++ linepikesstepview/src/main/java/com/anwesh/uiprojects/linepikesstepview/LinePikesStepView.kt
@@ -57,3 +57,21 @@ fun Canvas.drawLPSNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class LinePikesStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
