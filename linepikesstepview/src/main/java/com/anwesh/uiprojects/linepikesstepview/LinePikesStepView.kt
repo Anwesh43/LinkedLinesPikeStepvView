4ff8dc6eff72b29813fd4ca5612b433c2e072c1b
Anwesh43 <anweshthecool0@gmail.com>
2018-12-14 03:53:43 +0530

    adding libraries and defining constants

--- linepikesstepview/src/main/java/com/anwesh/uiprojects/linepikesstepview/LinePikesStepView.kt
+++ linepikesstepview/src/main/java/com/anwesh/uiprojects/linepikesstepview/LinePikesStepView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.linepikesstepview
 /**
  * Created by anweshmishra on 14/12/18.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val color : Int = Color.parseColor("#e74c3c")
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.5f
