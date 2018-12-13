78a83eceb184ab03410a4f27443f9f91628778cd
Anwesh43 <anweshthecool0@gmail.com>
2018-12-14 04:24:06 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedlinepikesstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinepikesstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlinepikesstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linepikesstepview.LinePikesStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinePikesStepView.create(this)
     }
 }
